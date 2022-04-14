a0d8e146f1336e6ec6a3b93f82e9be1071b540a2
J0seFY <joseignaciofuentesyanez@gmail.com>
2022-04-14 16:30:19 -0400

    se implemto metodo que suma polinomios y los multiplica

--- src/Polinomio.java
+++ src/Polinomio.java
@@ -1,26 +1,27 @@
 public class Polinomio {
 
-    Nodo head = null;
+    private Nodo head = null;
 
-    public Polinomio() {}
+    public Polinomio() {
+    }
 
-    public void addTermino(int coeficiente,int exponente){
+    public void addTermino(int coeficiente, int exponente) {
 
-        Nodo nuevoTermino = new Nodo(coeficiente,exponente);
-        if(head == null){
+        Nodo nuevoTermino = new Nodo(coeficiente, exponente);
+        if (head == null) {
             head = nuevoTermino;
-        }else {
+        } else {
             Nodo pointer = head;
-            if(nuevoTermino.exponente > head.exponente){
+            if (nuevoTermino.exponente > head.exponente) {
                 nuevoTermino.next = head;
                 head = nuevoTermino;
-            }else{
-                while(pointer.next != null) {
+            } else {
+                while (pointer.next != null) {
                     if (nuevoTermino.exponente > pointer.next.exponente) {
                         nuevoTermino.next = pointer.next;
                         pointer.next = nuevoTermino;
                         break;
-                    }else if(nuevoTermino.exponente == pointer.next.exponente){
+                    } else if (nuevoTermino.exponente == pointer.next.exponente) {
 
                         nuevoTermino.coeficiente += pointer.next.exponente;
                         nuevoTermino.next = pointer.next.next;
@@ -39,40 +40,72 @@ public class Polinomio {
 
     }
 
-    public int getGrado(){
+    public int getGrado() {
         return head.exponente;
     }
 
-    public int evaluar(int x){
+    public int evaluar(int x) {
         Nodo puntero = head;
         int total = 0;
-        while (puntero != null){
-            total += puntero.coeficiente*Math.pow(x, puntero.exponente);
+        while (puntero != null) {
+            total += puntero.coeficiente * Math.pow(x, puntero.exponente);
             puntero = puntero.next;
         }
         return total;
     }
 
-    public String toString(){
+    public void sumPolinomio(Polinomio polinomio) {
 
-        Nodo pointer = head;
-        String polinomioStr="";
-        while(pointer != null){
-            polinomioStr = polinomioStr + pointer.toString();
+        Nodo pointer = polinomio.head;
+        while (pointer != null) {
+            addTermino(pointer.coeficiente, pointer.exponente);
             pointer = pointer.next;
         }
 
+    }
+
+    public void multiPolinomio(Polinomio polinomio) {
+
+        if(head == null){
+            head = polinomio.head;
+        }else{
+
+            Nodo pointerNuevoP = polinomio.head;
+            Nodo pointer = head;
+            Polinomio nuevoPolinomio = new Polinomio();
+
+            while (pointerNuevoP != null){
+                while(pointer != null){
+                    nuevoPolinomio.addTermino(pointerNuevoP.coeficiente * pointer.coeficiente ,pointerNuevoP.exponente + pointer.exponente);
+                    pointer = pointer.next;
+                }
+                pointer = head;
+                pointerNuevoP = pointerNuevoP.next;
+            }
+
+            head = nuevoPolinomio.head;
+        }
+
+
+    }
+
+    public String toString() {
+
+        Nodo pointer = head;
+        String polinomioStr = "";
+        while (pointer != null) {
+            polinomioStr = polinomioStr + pointer.toString();
+            pointer = pointer.next;
+        }
+        if (polinomioStr.charAt(0) == '+') {
+            polinomioStr = polinomioStr.substring(1);
+        }
         return polinomioStr;
 
     }
 
 
-
-
-
-
-
-    private class Nodo{
+    private class Nodo {
 
         int coeficiente;
         int exponente;
@@ -92,26 +125,26 @@ public class Polinomio {
             this.next = next;
         }
 
-        public String toString(){
+        public String toString() {
             String signo = "+";
 
-            if(coeficiente ==0){
+            if (coeficiente == 0) {
                 return "";
             }
-            if(coeficiente < 0){
-                signo ="";
+            if (coeficiente < 0) {
+                signo = "";
             }
-            if(exponente == 0){
+            if (exponente == 0) {
                 return signo + coeficiente;
             }
-            if(exponente == 1){
+            if (exponente == 1) {
                 return signo + coeficiente + "x";
             }
 
-            return signo + coeficiente+"x^" + exponente;
+            return signo + coeficiente + "x^" + exponente;
         }
     }
-}
 
+}
 
 
--- src/TestPolinomio.java
+++ src/TestPolinomio.java
@@ -7,14 +7,16 @@ public class TestPolinomio {
 
         polinomio.addTermino(-3,4);
         polinomio.addTermino(-5,1);
-        polinomio.addTermino(2,2);
 
-
-        polinomio.addTermino(3,3);
-        polinomio.addTermino(3,2);
+        Polinomio polinomio2 = new Polinomio();
+        polinomio2.addTermino(3,2);
+        polinomio2.addTermino(5,0);
 
         System.out.println(polinomio.toString());
-        System.out.println(polinomio.getGrado());
-        System.out.println(polinomio.evaluar(2));
+        System.out.println(polinomio2.toString());
+
+        polinomio.multiPolinomio(polinomio2);
+
+        System.out.println(polinomio);
     }
 }
